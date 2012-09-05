2057860c7c33d03cfedb76480ddaf033cc241264
- <->
2012-09-05 08:55:12 -0700

    Add Logic for chronological index

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@
 dummy:
 
 all: sitemap.xml
+	perl work/blosxom.cgi
 
 sitemap.xml:
-	perl gen_sitemap.pl > sitemap.xml
+	perl work/gen_sitemap.pl > sitemap.xml
+	perl work/gen_chronological.pl > individual/source/chronological.txt
---
+++ work/gen_chronological.pl
@@ -0,0 +1,57 @@
+#!perl
+
+use 5.010;
+use strict;
+use warnings;
+use English qw( -no_match_vars );
+use autodie;
+use URI::Escape;
+
+
+my %post_by_date;
+open my $dates_fh, q{<}, 'plugins/state/dates';
+while (my $line = <$dates_fh>) {
+    chomp $line;
+    my ($julian, $file) = split /\s+/xms, $line, 2;
+    $post_by_date{$julian} = $file;
+}
+close $dates_fh;
+
+our $blog_title = 'Ocean of Awareness';
+our $url = 'http://jeffreykegler.github.com/Ocean-of-Awareness-blog/';
+
+sub interpolate {
+
+    package blosxom;
+    my $template = shift;
+    $template =~ s/(\$\w+(?:::)?\w*)/"defined $1 ? $1 : ''"/gee;
+    return $template;
+} ## end sub interpolate
+
+open my $head_fh, q{<}, 'source/head.html';
+my $header = join q{}, <$head_fh>;
+close $head_fh;
+print interpolate($header);
+
+my $previous_year;
+for my $julian (sort { $b <=> $a } keys %post_by_date) {
+    my $file = $post_by_date{$julian};
+    my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime($julian);
+    $year += 1900;
+    open my $post_fh, q{<}, $file;
+    my $title = <$post_fh>;
+    if (not defined $previous_year or $previous_year != $year) {
+        say "<h2>$year</h2>";
+	$previous_year = $year;
+    }
+    my @month_names = qw( January February March April May June
+	July August September October November December );
+	my $month_name = $month_names[$mon];
+	my $safe_file = uri_escape($file);
+    say qq{<p>$month_name $mday <a href="$safe_file">$title</a></p>};
+}
+  
+open my $foot_fh, q{<}, 'source/foot.html';
+print join q{}, <$foot_fh>;
+close $foot_fh;
+
