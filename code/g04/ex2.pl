a31deee2d272b4e67f900266fe5207d9ff247ca6
Jeffrey Kegler <jkegl@cpan.org>
2013-03-08 16:14:41 -0800

    Work on Go4 example

--- code/g04/ex2.pl
+++ code/g04/ex2.pl
@@ -20,11 +20,25 @@ use 5.010;
 use strict;
 use warnings;
 use English qw( -no_match_vars );
+use Data::Dumper;
 use GetOpt::Long;
 
 use Marpa::R2 2.047_012;
 
-die "$PROGRAM_NAME does not take arguments -- it simply runs a demo\n" if scalar @ARGV;
+sub usage {
+
+    die <<"END_OF_USAGE_MESSAGE";
+    $PROGRAM_NAME
+    $PROGRAM_NAME --verbose
+Runs a demo.
+By default, prints the only the result AST evaluation.
+With --verbose, dumps the AST.
+END_OF_USAGE_MESSAGE
+} ## end sub usage
+
+my $verbose_flag = 0;
+my $getopt_result = Getopt::Long::GetOptions( 'verbose!' => \$verbose_flag, );
+usage() if not $getopt_result;
 
 # Note Go4 ignores precedence
 my $rules = <<'END_OF_GRAMMAR';
@@ -49,39 +63,43 @@ END_OF_GRAMMAR
 
 my $grammar = Marpa::R2::Scanless::G->new(
     {   bless_package => 'Boolean_Expression',
-        source         => \$rules,
+        source        => \$rules,
     }
 );
 
 sub bnf_to_ast {
     my ($bnf) = @_;
     my $recce = Marpa::R2::Scanless::R->new( { grammar => $grammar } );
-    $recce->read(\ $bnf );
+    $recce->read( \$bnf );
     my $value_ref = $recce->value();
     if ( not defined $value_ref ) {
         die "No parse for $bnf";
     }
     return ${$value_ref};
-}
+} ## end sub bnf_to_ast
 
-
-my $ast1 = bnf_to_ast( q{1 and x or y and not x} );
+my $bnf = q{1 and x or y and not x};
+my $ast1 = bnf_to_ast($bnf);
+say qq{Boolean 1: "$bnf"};
+say Data::Dumper::Dumper($ast1) if $verbose_flag;
 my $context = Context->new();
 $context->assign( x => 0 );
 $context->assign( y => 1 );
 say $ast1->evaluate($context) ? 'true' : 'false';
 
-my $ast2 = bnf_to_ast( 'not z');
+$bnf = 'not z';
+my $ast2 = bnf_to_ast($bnf);
+say qq{Boolean 2: "$bnf"};
+say Data::Dumper::Dumper($ast2) if $verbose_flag;
 $context->assign( z => 1 );
 say $ast2->evaluate($context) ? 'true' : 'false';
 
 my $ast3 = $ast1->replace( 'y', $ast2 );
+say q{Boolean 1, with "y" replaced by Boolean 2};
+say Data::Dumper::Dumper($ast3) if $verbose_flag;
 say $ast3->evaluate($context) ? 'true' : 'false';
 
 exit 0;
-
-# say Data::Dumper::Dumper($value);
-# say Data::Dumper::Dumper($value->copy());
 
 package Context;
 
@@ -91,16 +109,17 @@ sub new {
 }
 
 sub assign {
-    my ($self, $name, $value) = @_;
+    my ( $self, $name, $value ) = @_;
     $self->{$name} = $value;
 }
 
 sub lookup {
-    my ($self, $name) = @_;
+    my ( $self, $name ) = @_;
     my $value = $self->{$name};
-    die qq{Attempt to read undefined boolean variable named "$name"} if not defined $value;
+    die qq{Attempt to read undefined boolean variable named "$name"}
+        if not defined $value;
     return $value;
-}
+} ## end sub lookup
 
 package Boolean_Expression::constant;
 
@@ -111,13 +130,13 @@ sub evaluate {
 }
 
 sub copy {
-    my ( $self ) = @_;
+    my ($self)  = @_;
     my ($value) = @{$self};
     return bless [$value], ref $self;
 }
 
 sub replace {
-    my ( $self) = @_;
+    my ($self) = @_;
     return $self->copy();
 }
 
@@ -134,17 +153,17 @@ sub evaluate {
 } ## end sub evaluate
 
 sub copy {
-    my ( $self ) = @_;
+    my ($self) = @_;
     my ($name) = @{$self};
     return bless [$name], ref $self;
 }
 
 sub replace {
     my ( $self, $name_to_replace, $expression ) = @_;
-    my ( $my_name ) = @{ $self };
+    my ($my_name) = @{$self};
     return $expression->copy() if $my_name eq $name_to_replace;
     return $self->copy();
-}
+} ## end sub replace
 
 package Boolean_Expression::not;
 
@@ -152,52 +171,55 @@ sub evaluate {
     my ( $self, $context ) = @_;
     my ($exp1) = @{$self};
     return !$exp1->evaluate($context);
-} ## end sub evaluate
+}
 
 sub copy {
-    my ( $self ) = @_;
-    return bless [map { $_->copy() } @{$self}], ref $self;
+    my ($self) = @_;
+    return bless [ map { $_->copy() } @{$self} ], ref $self;
 }
 
 sub replace {
     my ( $self, $name, $expression ) = @_;
-    return bless [map { $_->replace($name, $expression) } @{$self}], ref $self;
+    return bless [ map { $_->replace( $name, $expression ) } @{$self} ],
+        ref $self;
 }
 
 package Boolean_Expression::and;
 
 sub evaluate {
     my ( $self, $context ) = @_;
-    my ($exp1, $exp2) = @{$self};
+    my ( $exp1, $exp2 )    = @{$self};
     return $exp1->evaluate($context) && $exp2->evaluate($context);
-} ## end sub evaluate
+}
 
 sub copy {
-    my ( $self ) = @_;
-    return bless [map { $_->copy() } @{$self}], ref $self;
+    my ($self) = @_;
+    return bless [ map { $_->copy() } @{$self} ], ref $self;
 }
 
 sub replace {
     my ( $self, $name, $expression ) = @_;
-    return bless [map { $_->replace($name, $expression) } @{$self}], ref $self;
+    return bless [ map { $_->replace( $name, $expression ) } @{$self} ],
+        ref $self;
 }
 
 package Boolean_Expression::or;
 
 sub evaluate {
     my ( $self, $context ) = @_;
-    my ($exp1, $exp2) = @{$self};
+    my ( $exp1, $exp2 )    = @{$self};
     return $exp1->evaluate($context) || $exp2->evaluate($context);
-} ## end sub evaluate
+}
 
 sub copy {
-    my ( $self ) = @_;
-    return bless [map { $_->copy() } @{$self}], ref $self;
+    my ($self) = @_;
+    return bless [ map { $_->copy() } @{$self} ], ref $self;
 }
 
 sub replace {
     my ( $self, $name, $expression ) = @_;
-    return bless [map { $_->replace($name, $expression) } @{$self}], ref $self;
+    return bless [ map { $_->replace( $name, $expression ) } @{$self} ],
+        ref $self;
 }
 
 # vim: expandtab shiftwidth=4:
