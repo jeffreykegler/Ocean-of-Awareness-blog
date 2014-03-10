8c98833cd2b32d470fac6691deb1cdbe2630095e
Jeffrey Kegler <JKEGL@cpan.org>
2014-03-09 19:55:17 -0700

    New blog post

--- code/kv/kv_aleph.pl
+++ code/kv/kv_aleph.pl
@@ -2,19 +2,28 @@ use 5.010;
 use strict;
 use warnings;
 
-use Data::Dumper qw(Dumper);
+use Data::Dumper;
 use Marpa::R2 2.082000;
 
 my $input = <<'END_OF_STRING';
 {
+  name => 'test hash 1',
   42 => 'forty two',
  -42 => 'negative forty two',
  +42 => 'Perl 5 makes me disappear silently',
  key1 => 'normal Perl 5 key',
  key2 => 'another normal Perl 5 key',
- I am a very long key => 'Perl 5 syntax error',
- first key => 'Perl 5 syntax error',
- second key => 'Perl 5 syntax error',
+ third key => 'Perl 5 syntax error',
+}
+{
+ company name => 'Kamamaya Technology',
+ employee 1 => first name => 'Jane',
+ employee 1 => last name => 'Doe',
+ employee 1 => title => 'President',
+ employee 2 => first name => 'John',
+ employee 2 => last name => 'Smith',
+ employee 3 => first name => 'Clarence',
+ employee 3 => last name => 'Darrow',
 }
 END_OF_STRING
 
@@ -23,9 +32,10 @@ lexeme default = latm => 1 # handy boilerplate
 :default ::= action => ::array
 
 hashes ::= hash+
-hash ::= ('{') <key value pairs> ('}')
-<key value pairs> ::= <key value pair>* separator => comma
-<key value pair> ::= key ('=>') value
+hash ::= ('{') <keyed values> ('}') action => My_Actions::make_hash_ref
+<keyed values> ::= <keyed value>* separator => comma
+<keyed value> ::= <key series> ('=>') value action => My_Actions::kv_flatten
+<key series> ::= key+ separator => <wide comma> proper => 1
 key ::= word1 <other words maybe> action => My_Actions::make_key
 <other words maybe> ::= <other word>*
 value ::= string action => ::first
@@ -33,24 +43,61 @@ value ::= string action => ::first
 :discard ~ <ws chars>
 <ws chars> ~ [\s]+
 comma ~ ','
+<wide comma> ~ '=>'
 <other word> ~ <word chars>
 <string> ~ ['] <string chars maybe> [']
 <string chars maybe> ~ [^']*
 word1 ~ <good initial character> <word chars maybe>
-<good initial character> ~ [^$@%\s]
+# reserve some first characters for other uses
+<good initial character> ~ [^$@%\s[{<('"] # reserve some 
 <word chars> ~ [\w]+
 <word chars maybe> ~ [\w]*
 END_OF_SOURCE
 
-my $g = Marpa::R2::Scanless::G->new({ source => \$source});
-my $r = Marpa::R2::Scanless::R->new({ grammar => $g });
-$r->read(\$input);
+my $g = Marpa::R2::Scanless::G->new( { source  => \$source } );
+my $r = Marpa::R2::Scanless::R->new( { grammar => $g } );
+$r->read( \$input );
 my $value_ref = $r->value() or die "No parse";
-say Dumper($value_ref);
+say Data::Dumper::Dumper($value_ref);
 
 package My_Actions;
 
 sub make_key {
-   my (undef, $first_word, $other_words) = @_;
-   return join q{ }, $first_word, @{$other_words};
+    my ( undef, $first_word, $other_words ) = @_;
+    return join q{ }, $first_word, @{$other_words};
 }
+
+sub kv_flatten {
+    my ( undef, $keys, $value ) = @_;
+    return [ @{$keys}, $value ];
+}
+
+sub flatten {
+    my ( undef, @kv_pairs ) = @_;
+    return [ map { @{$_} } @kv_pairs ];
+}
+
+sub make_hash_ref {
+    my ( undef, $elements ) = @_;
+    my $hash_ref = {};
+    for my $element ( @{$elements} ) {
+        my @keyed_value = @{$element};
+        die "bad keyed value: ", Data::Dumper::Dumper($element)
+            if scalar @keyed_value < 2;
+        my $hash_at_depth = $hash_ref;
+        my $last_key_ix   = $#keyed_value - 1;
+        my $key;
+        for ( my $key_ix = 0; $key_ix < $last_key_ix; $key_ix++ ) {
+            $key = $keyed_value[$key_ix];
+            $hash_at_depth->{$key} = {} if not exists $hash_at_depth->{$key};
+            die "Attempt to use ", join q{->}, @keyed_value[ 0 .. $key_ix ],
+                "as both scalar and hash\n",
+                '  Element was ', Data::Dumper::Dumper($element)
+                if ref $hash_at_depth ne 'HASH';
+            $hash_at_depth = $hash_at_depth->{$key};
+        } ## end for ( my $key_ix = 0; $key_ix < $last_key_ix; $key_ix...)
+        $key = $keyed_value[$last_key_ix];
+        $hash_at_depth->{$key} = pop @keyed_value;
+    } ## end for my $element ( @{$elements} )
+    return $hash_ref;
+} ## end sub make_hash_ref
