7dfd63b35461ea38183c090fe0c65228da3e712b
Jeffrey Kegler <JKEGL@cpan.org>
2014-03-10 19:49:08 -0700

    New blog post

--- code/kv/kv_p5.pl
+++ code/kv/kv_p5.pl
@@ -4,11 +4,10 @@ use warnings;
 
 use Data::Dumper qw(Dumper);
 
-say Dumper({ 42 => 711,
+say Dumper({
+ 42 => 711,
  -42 => 711,
  +42 => 711,
  key1 => 711,
- key2 => 711,
- first key => 711,
- second key => 711,
+ # second key => 711,
 });
