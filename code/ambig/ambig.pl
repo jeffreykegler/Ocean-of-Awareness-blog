83d4c28b03d8f6386b87853558f57afed5d2c359
Jeffrey Kegler <JKEGL@cpan.org>
2014-08-18 10:12:01 -0700

    New blog post

--- code/ambig/ambig.pl
+++ code/ambig/ambig.pl
@@ -49,7 +49,7 @@ symbol ::= <symbol name> | <single quoted string>
 whitespace ~ [\s]+
 END_OF_DSL
 
-my $calc_lexer = q{Number matches '\d+'};
+my $calc_lexer   = q{Number matches '\d+'};
 my $calc_grammar = <<'END_OF_STRING';
 E ::= T '*' F
 E ::= T
@@ -58,28 +58,28 @@ T ::= Number
 END_OF_STRING
 chomp $calc_grammar;
 
-my $ex1 = join "\n", $calc_lexer, $calc_grammar, q{};
-my $ex2 = join "\n", $calc_grammar, $calc_lexer, q{};
-my $ex3 = join "\n", ($calc_grammar . q{ ;}), $calc_lexer, q{};
+my $ex1 = join "\n", $calc_lexer,   $calc_grammar, q{};
+my $ex2 = join "\n", $calc_grammar, $calc_lexer,   q{};
+my $ex3 = join "\n", ( $calc_grammar . q{ ;} ), $calc_lexer, q{};
 
 my $grammar = Marpa::R2::Scanless::G->new( { source => \$dsl } );
 
 TEST: {
-  my $input = \$ex1;
-  my $recce = Marpa::R2::Scanless::R->new( { grammar => $grammar } );
-  my $value_ref;
-  my $eval_ok = eval { $value_ref = doit($recce, $input); 1; };
-  if (!$eval_ok) {
-    Test::More::fail("Example 1 failed");
-    Test::More::diag( $EVAL_ERROR);
-    last TEST;
-  }
-  my $result = [grep { defined } split q{ }, ${$input}];
-  Test::More::is_deeply($result, flatten($value_ref), 'Example 1');
-}
+    my $input = \$ex1;
+    my $recce = Marpa::R2::Scanless::R->new( { grammar => $grammar } );
+    my $value_ref;
+    my $eval_ok = eval { $value_ref = doit( $recce, $input ); 1; };
+    if ( !$eval_ok ) {
+        Test::More::fail("Example 1 failed");
+        Test::More::diag($EVAL_ERROR);
+        last TEST;
+    }
+    my $result = [ grep {defined} split q{ }, ${$input} ];
+    Test::More::is_deeply( $result, flatten($value_ref), 'Example 1' );
+} ## end TEST:
 
 TEST: {
-  my $expected_error = <<'=== END ===';
+    my $expected_error = <<'=== END ===';
 Parse of BNF/Scanless source is ambiguous
 Length of symbol "Statement" at line 4, column 1 is ambiguous
   Choices start with: T ::= Number
@@ -88,38 +88,37 @@ Length of symbol "Statement" at line 4, column 1 is ambiguous
   Choice 2, length=33, ends at line 5, column 20
   Choice 2: T ::= Number\nNumber matches '\\d
 === END ===
-  my $input = \$ex2;
-  my $recce = Marpa::R2::Scanless::R->new( { grammar => $grammar
-     # , trace_terminals => 99
-  } );
-  my $value_ref;
-  my $eval_ok = eval { $value_ref = doit($recce, $input); 1; };
-  if (!$eval_ok) {
-    Test::More::is($EVAL_ERROR, $expected_error, 'Example 2 (ambiguous)');
-     last TEST;
-  }
-  Test::More::fail('Example 2 parsed -- it should not do so');
-}
+    my $input = \$ex2;
+    my $recce = Marpa::R2::Scanless::R->new( { grammar => $grammar } );
+    my $value_ref;
+    my $eval_ok = eval { $value_ref = doit( $recce, $input ); 1; };
+    if ( !$eval_ok ) {
+        Test::More::is( $EVAL_ERROR, $expected_error,
+            'Example 2 (ambiguous)' );
+        last TEST;
+    }
+    Test::More::fail('Example 2 parsed -- it should not do so');
+} ## end TEST:
 
 TEST: {
-  my $input = \$ex3;
-  my $recce = Marpa::R2::Scanless::R->new( { grammar => $grammar } );
-  my $value_ref;
-  my $eval_ok = eval { $value_ref = doit($recce, $input); 1; };
-  if (!$eval_ok) {
-    Test::More::fail('Example 3 failed');
-    Test::More::diag( $EVAL_ERROR);
-    last TEST;
-  }
-  my $result = [grep { defined } split q{ }, ${$input}];
-  Test::More::is_deeply($result, flatten($value_ref), 'Example 3');
-}
+    my $input = \$ex3;
+    my $recce = Marpa::R2::Scanless::R->new( { grammar => $grammar } );
+    my $value_ref;
+    my $eval_ok = eval { $value_ref = doit( $recce, $input ); 1; };
+    if ( !$eval_ok ) {
+        Test::More::fail('Example 3 failed');
+        Test::More::diag($EVAL_ERROR);
+        last TEST;
+    }
+    my $result = [ grep {defined} split q{ }, ${$input} ];
+    Test::More::is_deeply( $result, flatten($value_ref), 'Example 3' );
+} ## end TEST:
 
 sub flatten {
     my ($tree) = @_;
     if ( ref $tree eq 'REF' ) { return flatten( ${$tree} ); }
     if ( ref $tree eq 'ARRAY' ) {
-        return [ map { @{flatten($_)} } @{$tree} ];
+        return [ map { @{ flatten($_) } } @{$tree} ];
     }
     return [$tree];
 } ## end sub flatten
@@ -135,15 +134,15 @@ sub doit {
             "  the cause may be an unexpected event";
     } ## end if ( $length_read != length $input_length )
     if ( $recce->ambiguity_metric() > 1 ) {
-	# The calls in this section are experimental as of Marpa::R2 2.090
+
+        # The calls in this section are experimental as of Marpa::R2 2.090
         my $asf = Marpa::R2::ASF->new( { slr => $recce } );
         say STDERR 'No ASF' if not defined $asf;
         my $ambiguities = Marpa::R2::Internal::ASF::ambiguities($asf);
         my @ambiguities = grep {defined} @{$ambiguities}[ 0 .. 1 ];
         die
             "Parse of BNF/Scanless source is ambiguous\n",
-            Marpa::R2::Internal::ASF::ambiguities_show( $asf, \@ambiguities )
-        ;
+            Marpa::R2::Internal::ASF::ambiguities_show( $asf, \@ambiguities );
     } ## end if ( $recce->ambiguity_metric() > 1 )
 
     my $value_ref = $recce->value();
