837cf3ace74a28e660ff68c05263078ebb9570db
Jeffrey Kegler <jeffreykegler@cpan.org>
2018-06-08 09:24:45 -0400

    Start Pingali post

---
+++ metasources/pingali.pl
@@ -0,0 +1,111 @@
+#!perl
+
+use 5.010;
+use strict;
+use warnings;
+use English qw( -no_match_vars );
+use autodie;
+
+sub usage {
+	my ($message) = @_;
+	die $message, "\nusage: $PROGRAM_NAME";
+}
+
+my $fn_number = 0;
+my @fn_lines = ();
+my @lines = ();
+
+LINE: while ( my $line = <DATA> ) {
+    chomp $line;
+    if ( $line =~ /<footnote>/ ) {
+        do_footnote($line);
+	next LINE;
+    }
+    push @lines, $line;
+}
+
+my $output = join "\n", @lines;
+my $footnotes = join "\n", '<h2>Footnotes</h2>', @fn_lines;
+$output =~ s[<comment>FOOTNOTES HERE</comment>][$footnotes];
+
+say $output;
+
+sub do_footnote {
+    my ($line) = @_;
+    $fn_number++;
+    my $fn_ref = join '-', 'footnote', $fn_number, 'ref';
+    my $fn_href = join '-', 'footnote', $fn_number;
+    my $footnoted_line = $line;
+    $footnoted_line =~ s/<footnote>.*$//;
+    $footnoted_line .= qq{<a id="$fn_ref" href="#$fn_href">[$fn_number]</a>};
+    push @fn_lines, qq{<p id="$fn_href">$fn_number.};
+    $line =~ s/^.*<footnote>//;
+    my $inside_footnote = $line;
+    $inside_footnote =~ s/^.*<footnote>//;
+    push @fn_lines, $inside_footnote if $inside_footnote =~ m/\S/;
+    my $post_footnote = '';
+  FN_LINE: while ( my $fn_line = <DATA> ) {
+        chomp $fn_line;
+        if ( $fn_line =~ m[<\/footnote>] ) {
+	    $post_footnote = $fn_line;
+	    $post_footnote =~ s[^.*<\/footnote>][];
+	    $fn_line =~ s[</footnote>.*$][];
+	    push @fn_lines, $fn_line if $fn_line =~ m/\S/;
+	    push @fn_lines, qq{ <a href="#$fn_ref">&#8617;</a></p>};
+	    last FN_LINE;
+        }
+	push @fn_lines, $fn_line;
+    }
+    $footnoted_line .= $post_footnote;
+    push @lines, $footnoted_line if $footnoted_line =~ m/\S/;
+}
+
+__DATA__
+Derivates and pictures
+<html>
+  <head>
+  </head>
+  <body style="max-width:850px">
+    <!--
+      marpa_r2_html_fmt --no-added-tag-comment --no-ws-ok-after-start-tag
+      -->
+    <h2>Parsing with derivatives</h2>
+    <p>In a very cordial Twitter exchange with Matt Might and
+    and David Darais, Prof. Might asked why I was not looking at his
+    derivatives-based approach.
+    I answered that I was --
+    I see
+    Marpa as an optimized version of the Might/Darais approach.
+    </p>
+    <p>This may sound strange.
+    At first glance, the two algorithms seem about as different as
+    two algorithms that address the same problem space can
+    get.
+    My Marpa parser is an Earley parser, table-driven
+    with its core logic implemented in C language.
+    The Might/Darais/Spiewak parser is an extension of regular expressions
+    which constructs states on the fly,
+    and which deals that approaches speed issues by using memoization
+    and lazy evaluation and fixed points.
+    </p>
+    <h2>Parsing with pictures</h2>
+    <p>The key to seeing the common ground is a paper sent
+    me by Prof. Pingali at Austin.
+    (and why I see Marpa as an 
+    </p>
+    <h2>Comments, etc.</h2>
+    <p>
+      To learn about Marpa,
+      my Earley/Leo-based parsing project,
+      there is the
+      <a href="http://savage.net.au/Marpa.html">semi-official web site, maintained by Ron Savage</a>.
+      The official, but more limited, Marpa website
+      <a href="http://jeffreykegler.github.io/Marpa-web-site/">is my personal one</a>.
+      Comments on this post can be made in
+      <a href="http://groups.google.com/group/marpa-parser">
+        Marpa's Google group</a>,
+      or on our IRC channel: #marpa at freenode.net.
+    </p>
+    <comment>FOOTNOTES HERE</comment>
+  </body>
+</html>
