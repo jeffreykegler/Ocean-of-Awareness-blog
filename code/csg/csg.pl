7cf2119d07c3269a52ab88d15f36ef481b283ab5
Jeffrey Kegler <jeffreykegler@cpan.org>
2018-06-11 21:32:40 -0400

    Work on new CSG post

--- code/csg/csg.pl
+++ code/csg/csg.pl
@@ -7,12 +7,12 @@ use warnings;
 use Data::Dumper;
 use English qw( -no_match_vars );
 
-use Test::More tests => 6;
+use Test::More tests => 12;
 
-use Marpa::R2 2.090;
+use Marpa::R2 4.000;
 
 my $dsl = <<'END_OF_DSL';
-:default ::= action => [name,values]
+:default ::= action => [name,start,length,values]
 lexeme default = latm => 1
 S ::= prefix ABC trailer
 ABC ::= ABs Cs
@@ -26,40 +26,77 @@ Cs ~ 'c'
 C_extra ~ 'c'
 END_OF_DSL
 
-my @ex = ('abc',
- 'aabbcc',
- 'aaabccc',
- 'aabbcccc',
- 'aabbccc',
- 'aaaaabbccc',
+my @test = (
+ [ 'abc', 'OK', 'ABC of length 3 starts at 0' ],
+ [ 'aabbcc', 'OK', 'ABC of length 6 starts at 0' ],
+ [ 'aaabccc', 'OK', 'ABC of length 3 starts at 2' ],
+ [ 'aabbcccc', 'OK', 'ABC of length 6 starts at 0' ],
+ [ 'aabbccc', 'OK', 'ABC of length 6 starts at 0' ],
+ [ 'aaaaabbccc', 'OK', 'ABC of length 6 starts at 3' ],
+ [ 'abbc', "Too few A's", '[fail]' ],
+ [ 'aabbbccc', "Too few A's", '[fail]' ],
+ [ 'aabbc', "Too few C's", '[fail]' ],
+ [ 'aaabbbc', "Too few C's", '[fail]' ],
 );
 
 my $grammar = Marpa::R2::Scanless::G->new( { source => \$dsl } );
 
-TEST: for my $ix (0 .. $#ex)
+TEST: for my $ix (0 .. $#test)
 {
-    my $input = \($ex[$ix]);
+    my ($input, $expected_result, $expected_value) = @{$test[$ix]};
     my $i = $ix + 1;
-    say "** Test #$i: ", ${$input};
+    say "** Test #$i: ", $input;
     my $recce = Marpa::R2::Scanless::R->new( { grammar => $grammar } );
     my $value_ref;
-    my $eval_ok = eval { $value_ref = doit( $recce, $input ); 1; };
+    my $result = 'OK';
+    my $eval_ok = eval { $value_ref = doit( $recce, \$input ); 1; };
     if ( !$eval_ok ) {
-        Test::More::fail("Example $i failed");
-        Test::More::diag($EVAL_ERROR);
-	next TEST;
+	my $eval_error = $EVAL_ERROR;
+	PARSE_EVAL_ERROR: {
+	  if ($eval_error =~ m/No lexeme found/) {
+	     $result = "Too few A's";
+	     last PARSE_EVAL_ERROR;
+	  }
+	  if ($eval_error =~ m/Too few C's/) {
+	     $result = "Too few C's";
+	     last PARSE_EVAL_ERROR;
+	  }
+	  $result = "Error: $EVAL_ERROR";
+	  Test::More::diag($result);
+	}
     }
-    say flatten($value_ref);
-    Test::More::pass( "Example $i OK" );
+    if ($result ne $expected_result) {
+        Test::More::fail(qq{Result of "$input" "$result"; expected "$expected_result"});
+    } else {
+      Test::More::pass(qq{Result of "$input" matches});
+    }
+    my $value = '[fail]';
+    if ($value_ref) {
+       $value = find_ABC($value_ref) // '[none]';
+    }
+    if ($value ne $expected_value) {
+        Test::More::fail(qq{Test of "$input" value was "$value"; expected "$expected_value"});
+    } else {
+      Test::More::pass(qq{Value of "$input" matches});
+    }
 } ## end TEST:
 
-sub flatten {
+sub find_ABC {
     my ($tree) = @_;
-    if ( ref $tree eq 'REF' ) { return flatten( ${$tree} ); }
-    if ( ref $tree eq 'ARRAY' ) {
-        return join " ", map { flatten($_) } @{$tree};
+    my $ref_type = ref $tree;
+    return find_ABC(${$tree}) if $ref_type eq 'REF';
+    return undef if ref $tree ne 'ARRAY';
+    # say STDERR "tree: ", join " ", @{$tree};
+    if ($tree->[0] eq 'ABC') {
+       my $length = $tree->[2];
+       my $start = $tree->[1];
+       return "ABC of length $length starts at $start";
     }
-    return $tree;
+    for (my $i = 0; $i <= $#{$tree}; $i++) {
+	my $child_value = find_ABC($tree->[$i]);
+	return $child_value if $child_value;
+    }
+    return undef;
 } ## end sub flatten
 
 sub doit {
@@ -89,9 +126,9 @@ sub doit {
                 if ( substr( ${$input}, $pos, $c_length ) eq $c_seq ) {
                     # say STDERR "Event $name NYI Match!";
                     $recce->lexeme_read( 'Cs', $pos, $c_length, $c_seq );
-                    next EVENT;
+		    next EVENT;
                 }
-                die "Event $name NYI no match";
+		die qq{Too few C's};
             }
         }
     }
