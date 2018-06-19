9ee6865303ba1ac0fc4ab3735596c7fbe0e68f30
Jeffrey Kegler <jeffreykegler@cpan.org>
2018-06-19 15:59:26 -0400

    Work on left recursion post

--- metasources/lrecursion.pl
+++ metasources/lrecursion.pl
@@ -131,11 +131,139 @@ Why the big deal about left recursion?
     in terms of 
     <tt>&lt;A&gt;</tt> --
     you have an infinite regress.
-    Left recursions are a particular problem because almost all practical<footnote>
+    All recursions can be a problem,
+    but left recursions are a particular problem because almost all practical
+    parsing methods<footnote>
+    I probably could have said "all practical parsing methods"
+    instead of "almost all".
+    Right-to-left parsing methods exist,
+    but they see little use,
+    in any case, they only reverse the problem.
+    Parsing in both directions is certainly possible but,
+    as I will show,
+    we do not have to go to that much trouble.
     </footnote>
-    parsing methods proceed left to right,
-    and derivations like <tt>(8)</tt> will lead them straight into
+    proceed left to right,
+    and derivations like <tt>(8)</tt> will lead many of
+    the most popular algorithms straight into
     an infinite regress.
+    <h2>Why do some algorithms not have a problem?</h2>
+    <p>In a sense,
+    all algorithms which solve the left recursion problem do
+    it in the same way.
+    It is just that in some,
+    the solution appears in a much simpler form.
+    </p>
+    <p>
+    The solution is at most simple in Earley's algorithm.
+    That is no coincidence -- as Pingali and Bernadi show,
+    Earley's, despite its daunting reputation,
+    is actually the most basic context-free parsing algorithm.
+    </p>
+    <p>Earley's builds a table.
+    The Earley table contains an initial Earley set
+    and an Earley set for each token.
+    Each Earley sets
+    describes that state of the parse after consuming that token.
+    The basic idea is not dissimilar to that of MDS,
+    and the logic for building the Earley sets resembles
+    that of MDS.<footnote>
+    TODO.
+    </footnote>
+    <p>
+    For our purposes, what matter is that
+    each Earley set contains items.
+    Some of the items are called predictions
+    because they predict the occurrence of a symbol
+    at that location in the input.
+    Left recursions are recorded as predictions of the left
+    recursive symbol.
+    And that, simple as it is,
+    is the Earley's solution to left recursion.
+    </p>
+    Multiple occurrences of a prediction item would be identical,
+    and therefore useless.
+    Subsequent attempts
+    to add the same prediction item are ignored,
+    and recursion does not occur.
+    </p>
+    <h2>If some do not, why do the others?</h2>
+    <p>Besides Earley's,
+    a number of other algorithms handle left recursion without
+    any issue -- notably LALR 
+    (aka <tt>yacc</tt> or <tt>bison</tt>) and LR.
+    This re-raises the original question:
+    why do some algorithms have a left recursion problem?
+    </p>
+    <p>The worst afflicted algorithms are the "top-down"
+    parsers.
+    The best known of these is recursive descent --
+    a parsing methodology which, essentially, does parsing
+    by calling a subroutine to handle each symbol.
+    In a naive implementation of recursive descent,
+    left recursion is very problematic.
+    To illustrate, if you are writing the function <tt>parse_A()</tt>
+    and have a rule
+    <pre><tt>
+    (9) A ::= A B
+    </tt></pre>
+    the first thing you need do in
+    <tt>parse_A()</tt>.
+    is to call <tt>parse_A()</tt>.
+    <h2>The fixed-point solution to left recursion</h2>
+    <p>Over the years,
+    many ways to solve the top-down left recursion issue have been
+    announced.
+    Recently, I came across one of the more interesting --
+    interesting because it describes all the others,
+    including the Earley algorithm solution.
+    </p>
+    <p>
+    Might, Darais and Spiewak (MDS) reduce the problem to that
+    the more general one of finding a "fixed point" of the recursion.
+    </p>
+    <p>In math, the "fixed point" of a function is an argument of
+    the function which is equal to its value for that argument --
+    that is, an <tt>x</tt> such that <tt>f(x) = x</tt>.
+    MDS describe an algorithm which "solves" the left recursion
+    for its fixed point.
+    That "fixed point" can then be memoized.
+    For example the value of <tt>parse_A</tt>
+    can be the memoized "fixed point" value of
+    <tt>&lt;A&gt;</tt>.
+    </p>
+    <p>Abstractly,
+    the computation of an Earley is the application of a set
+    of rules for adding Earley items.
+    This continues until no more Earley items can be added.
+    In other words, the rules for building an Earley set
+    are applied until they find
+    their "fixed point".<footnote>
+    Recall that potential left recursions are 
+    recorded as "predictions" in Earley's algorithm.
+    Predictions recurse,
+    but since they do not depend on the input,
+    they can be precomputed.
+    This means that Earley implementations can
+    bring each Earley set to its fixed point
+    very quickly.
+    </footnote>
+    </p>
+    <h2>Other top-down solutions</h2>
+    <p>The MDS fixed point solution <b>does</b>
+    the job,
+    but as described in their paper it requires a functional
+    programming language to implement,
+    and is not inexpensive.
+    There have been many other attempted top-down solutions
+    to the left-recursion problem,
+    some with a degree of success.
+    In retrospect,
+    perhaps these can
+    be seen as approaches to,
+    and usually oversimplifications of,
+    the MDS fixed point approach.
+    </p>
     <h2>The code, comments, etc.</h2>
       To learn more about Marpa,
       a good first stop is the
