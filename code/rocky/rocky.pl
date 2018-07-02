bf0c0c4305ecd9dc01afc459eab603100751fb10
Jeffrey Kegler <jeffreykegler@cpan.org>
2018-07-02 13:37:27 -0400

    Work on Rocky's grammar

--- code/rocky/rocky.pl
+++ code/rocky/rocky.pl
@@ -7,44 +7,50 @@ use warnings;
 use Data::Dumper;
 use English qw( -no_match_vars );
 
-use Test::More tests => 24;
+use Test::More tests => 12;
 
 use Marpa::R2 4.000;
 
 my $dsl = <<'END_OF_DSL';
 lexeme default = latm => 1
-:default ::= action => [name,start,length,values]
+:default ::= action => [name,values]
 range ::= location
 range ::= location COMMA NUMBER
 range ::= location COMMA OFFSET
 range ::= COMMA location
 range ::= location COMMA
-range ::= location
-range ::= DIRECTION
+range ::= direction
+
+direction ::= DIRECTION
 
 # Note no space is allowed between FILENAME and NUMBER
-location    ::= FILENAME COLON NUMBER
+location    ::= FILE_LINE
 location    ::= FUNCNAME
 # If just a number is given, the the filename is implied
 location    ::= NUMBER
-location    ::= METHOD
-location    ::= OFFSET
+location    ::= method
+method      ::= METHOD
+location    ::= offset
+offset      ::= OFFSET
 
 :discard ~ whitespace
-FILE_LINE ::= FILENAME COLON number
-COMMA ~ ':'
+FILE_LINE ~ FILENAME COLON number
+COLON ~ ':'
 COMMA ~ ','
 NUMBER ~ number
-number ~ \d+
-OFFSET ~ '+' \d+
+OFFSET ~ '+' digits
+number ~ digits
+digits ~ [\d]+
 DIRECTION ~ '+'
 DIRECTION ~ '-'
 whitespace ~ [\s]+
 
-# Could not find definitions of FILENAME, FUNCNAME
+# Could not find definitions for these lexemes:
+# FILENAME, FUNCNAME
 # This will do for now
 FILENAME ~ name
-FUNCNAME ~ name
+FUNCNAME ~ name '()'
+METHOD ~ name
 name ~ name_first_char name_later_chars
 name_first_char ~ [A-Za-z_]
 name_later_chars ~ name_later_char+
@@ -52,18 +58,12 @@ name_later_char ~ [\w]
 END_OF_DSL
 
 my @test = (
- [ 'abc', 'OK', 'ABC of length 3 starts at 0' ],
- [ 'aabbcc', 'OK', 'ABC of length 6 starts at 0' ],
- [ 'aaabccc', 'OK', 'ABC of length 3 starts at 2' ],
- [ 'aabbcccc', 'OK', 'ABC of length 6 starts at 0' ],
- [ 'aabbccc', 'OK', 'ABC of length 6 starts at 0' ],
- [ 'aaaaabbccc', 'OK', 'ABC of length 6 starts at 3' ],
- [ 'aaaabbbccc', 'OK', 'ABC of length 9 starts at 1' ],
- [ 'abbc', "Too few A's or no B's", '[fail]' ],
- [ 'aabbbccc', "Too few A's or no B's", '[fail]' ],
- [ 'aabbc', "Too few C's", '[fail]' ],
- [ 'aaabbbc', "Too few C's", '[fail]' ],
- [ 'aacc', "Too few A's or no B's", '[fail]' ],
+    [ 'abc', 'OK', [ 'range', [ 'location',  [ 'method', 'abc' ] ] ] ],
+    [ '+',   'OK', [ 'range', [ 'direction', '+' ] ] ],
+    [ '+9', 'OK', [ 'range', [ 'location', [ 'offset', '+9' ] ] ] ],
+    [ 'xyz:3,9', 'OK', [ 'range', [ 'location', 'xyz:3' ], ',', '9' ] ],
+    [ ',42',     'OK', [ 'range', ',', [ 'location', '42' ] ] ],
+    [ '42,', 'OK', [ 'range', [ 'location', '42' ], ',' ] ],
 );
 
 my $grammar = Marpa::R2::Scanless::G->new( { source => \$dsl } );
@@ -90,39 +90,24 @@ TEST: for my $ix (0 .. $#test)
       Test::More::pass(qq{Result of "$input" matches});
     }
     my $value = '[fail]';
+    my $dump_expected = '[fail]';
     if ($value_ref) {
-       $value = find_ABC($value_ref) // '[none]';
+        $value         = Data::Dumper::Dumper($value_ref);
+        $dump_expected = Data::Dumper::Dumper(\$expected_value);
     }
-    if ($value ne $expected_value) {
-        Test::More::fail(qq{Test of "$input" value was "$value"; expected "$expected_value"});
+    if ($value ne $dump_expected) {
+        Test::More::fail(qq{Test of "$input" value was "$value"; expected "$dump_expected"});
     } else {
       Test::More::pass(qq{Value of "$input" matches});
     }
 } ## end TEST:
-
-sub find_ABC {
-    my ($tree) = @_;
-    my $ref_type = ref $tree;
-    return find_ABC(${$tree}) if $ref_type eq 'REF';
-    return undef if ref $tree ne 'ARRAY';
-    if ($tree->[0] eq 'ABC') {
-       my $length = $tree->[2];
-       my $start = $tree->[1];
-       return "ABC of length $length starts at $start";
-    }
-    for (my $i = 0; $i <= $#{$tree}; $i++) {
-	my $child_value = find_ABC($tree->[$i]);
-	return $child_value if $child_value;
-    }
-    return undef;
-}
 
 sub doit {
     my ( $recce, $input ) = @_;
     my $input_length = length ${$input};
     my $pos = $recce->read($input);
     if ( $pos < $input_length ) {
-	die qq{Unexpected event: name="$name"};
+	die sprintf qq{Unfinished parse: remainder="%" }, substr(${$input}, $pos);
     }
     my $value_ref = $recce->value();
     if ( !$value_ref ) {
