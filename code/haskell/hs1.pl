ae7b44e966297ddc409dc52c4c982657584317c5
Jeffrey Kegler <jeffreykegler@cpan.org>
2018-08-07 12:38:44 -0400

    Work on Haskell parsing

--- code/haskell/hs1.pl
+++ code/haskell/hs1.pl
@@ -238,9 +238,126 @@ qvar ::= qvarid
 # qop	→	qvarop | qconop	    (qualified operator)
 # gconsym	→	: | qconsym
 
+# Lexical syntax from Section 10.2
+
+# prgram	→	{ lexeme | whitespace }
+# lexeme	→	qvarid | qconid | qvarsym | qconsym
+# |	literal | special | reservedop | reservedid
+# literal	→	integer | float | char | string
+# special	→	( | ) | , | ; | [ | ] | ` | { | }
+#  
+# whitespace	→	whitestuff {whitestuff}
+# whitestuff	→	whitechar | comment | ncomment
+# whitechar	→	newline | vertab | space | tab | uniWhite
+# newline	→	return linefeed | return | linefeed | formfeed
+# return	→	a carriage return
+# linefeed	→	a line feed
+# vertab	→	a vertical tab
+# formfeed	→	a form feed
+# space	→	a space
+# tab	→	a horizontal tab
+# uniWhite	→	any Unicode character defined as whitespace
+#  
+# comment	→	dashes [ any⟨symbol⟩ {any} ] newline
+# dashes	→	-- {-}
+# opencom	→	{-
+# closecom	→	-}
+# ncomment	→	opencom ANY seq {ncomment ANY seq} closecom
+# ANY seq	→	{ANY }⟨{ANY } ( opencom | closecom ) {ANY }⟩
+# ANY	→	graphic | whitechar
+# any	→	graphic | space | tab
+# graphic	→	small | large | symbol | digit | special | " | '
+#  
+# small	→	ascSmall | uniSmall | _
+
+small ~ ascSmall | '_'
+
+# ascSmall	→	a | b | … | z
+
+ascSmall ~ [a-z]
+
+# uniSmall	→	any Unicode lowercase letter
+#  
+# large	→	ascLarge | uniLarge
+
+large ~ ascLarge
+
+# ascLarge	→	A | B | … | Z
+
+ascLarge ~ [A-Z]
+
+# uniLarge	→	any uppercase or titlecase Unicode letter
+# symbol	→	ascSymbol | uniSymbol⟨special | _ | " | '⟩
+#  
+# ascSymbol	→	! | # | $ | % | & | ⋆ | + | . | / | < | = | > | ? | @
+# |	\ | ^ | | | - | ~ | :
+# uniSymbol	→	any Unicode symbol or punctuation
+# digit	→	ascDigit | uniDigit
+# ascDigit	→	0 | 1 | … | 9
+# uniDigit	→	any Unicode decimal digit
+# octit	→	0 | 1 | … | 7
+# hexit	→	digit | A | … | F | a | … | f
+#  
+# varid	→	(small {small | large | digit | ' })⟨reservedid⟩
+
+varid ~ small noninitial_id_chars
+noninitial_id_chars ~ small | large | digit | [']
+
+# conid	→	large {small | large | digit | ' }
+# reservedid	→	case | class | data | default | deriving | do | else
+# |	foreign | if | import | in | infix | infixl
+# |	infixr | instance | let | module | newtype | of
+# |	then | type | where | _
+#  
+# varsym	→	( symbol⟨:⟩ {symbol} )⟨reservedop | dashes⟩
+# consym	→	( : {symbol})⟨reservedop⟩
+# reservedop	→	.. | : | :: | = | \ | | | <- | -> |  @ | ~ | =>
+#  
+# varid	    	    (variables)
+# conid	    	    (constructors)
+# tyvar	→	varid	    (type variables)
+# tycon	→	conid	    (type constructors)
+# tycls	→	conid	    (type classes)
+# modid	→	{conid .} conid	    (modules)
+
+modid ~ conid | modid '.' conid
+
+#  
+# qvarid	→	[ modid . ] varid
+
+qvarid ~ modid '.' varid | varid
+
+# qconid	→	[ modid . ] conid
+# qtycon	→	[ modid . ] tycon
+# qtycls	→	[ modid . ] tycls
+# qvarsym	→	[ modid . ] varsym
+# qconsym	→	[ modid . ] consym
+#  
+# decimal	→	digit{digit}
+# octal	→	octit{octit}
+# hexadecimal	→	hexit{hexit}
+#  
+# integer	→	decimal
+# |	0o octal | 0O octal
+# |	0x hexadecimal | 0X hexadecimal
+# float	→	decimal . decimal [exponent]
+# |	decimal exponent
+# exponent	→	(e | E) [+ | -] decimal
+#  
+# char	→	' (graphic⟨' | \⟩ | space | escape⟨\&⟩) '
+# string	→	" {graphic⟨" | \⟩ | space | escape | gap} "
+# escape	→	\ ( charesc | ascii | decimal | o octal | x hexadecimal )
+# charesc	→	a | b | f | n | r | t | v | \ | " | ' | &
+# ascii	→	^cntrl | NUL | SOH | STX | ETX | EOT | ENQ | ACK
+# |	BEL | BS | HT | LF | VT | FF | CR | SO | SI | DLE
+# |	DC1 | DC2 | DC3 | DC4 | NAK | SYN | ETB | CAN
+# |	EM | SUB | ESC | FS | GS | RS | US | SP | DEL
+# cntrl	→	ascLarge | @ | [ | \ | ] | ^ | _
+# gap	→	\ whitechar {whitechar} \
+
 END_OF_DSL
 
-my i$nput = <<'EOS';
+my $input = <<'EOS';
 module AStack( Stack, push, pop, top, size ) where  
 data Stack a = Empty  
              | MkStack a (Stack a)  
@@ -271,7 +388,7 @@ my $grammar = Marpa::R2::Scanless::G->new( { source => \$dsl } );
     } );
     my $value_ref;
     my $result = 'OK';
-    my $eval_ok = eval { $value_ref = doit( $recce, \$test ); 1; };
+    my $eval_ok = eval { $value_ref = doit( $recce, \$input ); 1; };
     if ( !$eval_ok ) {
 	my $eval_error = $EVAL_ERROR;
 	PARSE_EVAL_ERROR: {
@@ -311,7 +428,7 @@ sub doit {
             $event_ix++
           )
         {
-            # my $name = $event->[0];
+            my $name = $event->[0];
 	    die qq{Unexpected event: name="$name"};
         }
     }
