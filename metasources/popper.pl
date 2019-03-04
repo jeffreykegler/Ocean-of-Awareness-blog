d1f07de708f2bc289de96f599abf5458b1829a73
Jeffrey Kegler <jeffreykegler@cpan.org>
2019-03-04 14:28:38 -0500

    Start Popper blog post

---
+++ metasources/popper.pl
@@ -0,0 +1,112 @@
+#!perl
+
+use 5.010;
+use strict;
+use warnings;
+use English qw( -no_match_vars );
+use autodie;
+
+sub usage {
+	my ($message) = @_;
+	die $message, "\nusage: $PROGRAM_NAME";
+}
+
+my $fn_number = 0;
+my @fn_lines = ();
+my @lines = ();
+
+LINE: while ( my $line = <DATA> ) {
+    chomp $line;
+    if ( $line =~ /<footnote>/ ) {
+        do_footnote($line);
+	next LINE;
+    }
+    push @lines, $line;
+}
+
+my $output = join "\n", @lines;
+my $footnotes = join "\n", '<h2>Footnotes</h2>', @fn_lines;
+$output =~ s[<comment>FOOTNOTES HERE</comment>][$footnotes];
+
+say $output;
+
+sub do_footnote {
+    my ($line) = @_;
+    $fn_number++;
+    my $fn_ref = join '-', 'footnote', $fn_number, 'ref';
+    my $fn_href = join '-', 'footnote', $fn_number;
+    my $footnoted_line = $line;
+    $footnoted_line =~ s/<footnote>.*$//;
+    $footnoted_line .= qq{<a id="$fn_ref" href="#$fn_href">[$fn_number]</a>};
+    push @fn_lines, qq{<p id="$fn_href"><b>$fn_number.</b>};
+    $line =~ s/^.*<footnote>//;
+    my $inside_footnote = $line;
+    $inside_footnote =~ s/^.*<footnote>//;
+    push @fn_lines, $inside_footnote if $inside_footnote =~ m/\S/;
+    my $post_footnote = '';
+  FN_LINE: while ( my $fn_line = <DATA> ) {
+        chomp $fn_line;
+        if ( $fn_line =~ m[<\/footnote>] ) {
+	    $post_footnote = $fn_line;
+	    $post_footnote =~ s[^.*<\/footnote>][];
+	    $fn_line =~ s[</footnote>.*$][];
+	    push @fn_lines, $fn_line if $fn_line =~ m/\S/;
+	    push @fn_lines, qq{ <a href="#$fn_ref">&#8617;</a></p>};
+	    last FN_LINE;
+        }
+	push @fn_lines, $fn_line;
+    }
+    $footnoted_line .= $post_footnote;
+    push @lines, $footnoted_line if $footnoted_line =~ m/\S/;
+}
+
+__DATA__
+Karl Popper and Parsing
+<html>
+  <head>
+  </head>
+  <body>
+    <blockquote>
+	Let me repeat this important point.
+	Scientific theories are not just the
+	results of observation.
+	They are, in the main, the products
+	of myth-making and of tests.
+	Tests proceed partly by way of observation,
+	and observation is thus very important;
+	but its function is not that of producing theories.
+	It plays its role in
+	rejecting, eliminating, and criticizing theories;
+	and it challenges us to produce new myths,
+	new theories which may stand up to these
+	observational tests.<footnote>
+	Karl Popper, <cite>Conjectures and Refutations</cite>, 
+	Routledge and kegan Paul, 2002,
+	p. 172.
+	</footnote>
+    </blockquote>
+    <h2>Karl Popper and parsing</h2>
+    <p>
+      <!--
+      marpa_r2_html_fmt --no-added-tag-comment --no-ws-ok-after-start-tag
+      -->
+    <h2>Comments, etc.</h2>
+    <p>
+      The background material for this post is in my
+    <a href="https://jeffreykegler.github.io/personal/timeline_v3">
+    Parsing: a timeline 3.0</a>,
+    and this post may be considered a supplement to "Timelime".
+      To learn about Marpa,
+      my Earley/Leo-based parsing project,
+      there is the
+      <a href="http://savage.net.au/Marpa.html">semi-official web site, maintained by Ron Savage</a>.
+      The official, but more limited, Marpa website
+      <a href="http://jeffreykegler.github.io/Marpa-web-site/">is my personal one</a>.
+      Comments on this post can be made in
+      <a href="http://groups.google.com/group/marpa-parser">
+        Marpa's Google group</a>,
+      or on our IRC channel: #marpa at freenode.net.
+    </p>
+    <comment>FOOTNOTES HERE</comment>
+  </body>
+</html>
