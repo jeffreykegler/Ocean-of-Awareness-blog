c45fcec87d479c3559e350ce1f8a1584635a2121
Jeffrey Kegler <jeffreykegler@cpan.org>
2019-03-10 17:49:16 -0400

    Work on Popper blog post

--- metasources/popper.pl
+++ metasources/popper.pl
@@ -61,129 +61,228 @@ sub do_footnote {
 }
 
 __DATA__
-The open Timeline and its critics
+My "Timeline of Parsing", and its crime
 <html>
   <head>
   </head>
   <body>
-      <!--
+    <!--
       marpa_r2_html_fmt --no-added-tag-comment --no-ws-ok-after-start-tag
       -->
-    <blockquote>
-	Let me repeat this important point.
-	Scientific theories are not just the
-	results of observation.
-	They are, in the main, the products
-	of myth-making and of tests.
-	Tests proceed partly by way of observation,
-	and observation is thus very important;
-	but its function is not that of producing theories.
-	It plays its role in
-	rejecting, eliminating, and criticizing theories;
-	and it challenges us to produce new myths,
-	new theories which may stand up to these
-	observational tests.<footnote>
-	Karl Popper, <cite>Conjectures and Refutations</cite>, 
-	Routledge and kegan Paul, 2002,
-	p. 172.
-	</footnote>
+    <blockquote>Always approach a case with an absolutely blank mind.
+      It is always an advantage.
+      Form no theories, just simply observe and draw inferences from your observations.
+      &mdash;
+      <cite>
+        Sherlock Holmes, quoted in "The Adventure of the Cardboard Box"<cite>.
+        </cite></cite></blockquote>
+    <blockquote>It is a capital mistake to theorize before one has data.
+      &mdash;
+      <cite>
+        Holmes, in "A Scandal in Bohemia"</cite>.
     </blockquote>
-    <h2>The open timeline and its enemies</h2>
+    <blockquote>I make a point of never having any prejudices, and of following docilely wherever fact may lead me.
+      &mdash;
+      <cite>"The Reigate Puzzle"</cite>.
+    </blockquote>
+    <blockquote>When you have eliminated the impossible, whatever remains, no matter how improbable, must be the truth.
+      &mdash;
+      <cite>"The Sign of Four"</cite>.
+    </blockquote>
+    <blockquote>
+      In imagination there
+      exists the perfect
+      mystery story.
+      Such a story presents
+      the essential clues, and compels us to form our own
+      theory of the case.
+      If we
+      follow the plot carefully, we arrive at the complete
+      all
+      solution for ourselves just before the author's disclosure
+      at the end of the book. The solution itself, contrary to
+      those of inferior mysteries, does not disappoint us; more-
+      over, it appears at the very moment we expect it.
+      Can we liken the reader of such a book to the scientists,
+      who throughout successive generations continue to seek
+      solutions of the mysteries in the book of nature? The
+      comparison is false and will have to be abandoned later,
+      but it has a modicum of justification which may be
+      extended and modified to make it more appropriate to
+      the endeavour of science to solve the mystery of the
+      universe.
+      &mdash;
+      <cite>Albert Einstein
+        and Leopold Infeld</cite>.<footnote>
+        Page 3,
+        <cite>The Evolution of Physics</cite>,
+        Scientific Book Club, London, 1938?</footnote>
+    </blockquote>
+    <blockquote>
+      The scientist must, at least in part,
+      commit his own crime, as well as carry out the investigation.
+      &mdash;
+      <cite>Einstein and Infeld</cite>.
+      <footnote>
+        "The Evolution of Physics", p. 78.
+      </footnote>
+    </blockquote>
+    <h2>The timeline and its critics</h2>
     <p>I've written a timeline history of parsing theory
-    which is, despite being very technical,
-    is by far my most popular writing.
-    Despite, or because, of this,
-    it is not without its critics.
-    Many who praise it highly also have severe criticisms.
+      which, even thought it is very technical,
+      is by far my most popular writing.
+      Despite, or because, of this,
+      it is not without its critics.
+      The most central criticisms are lack of objectivity,
+      and bias.
+      In this post, I will take a careful look at methodology,
+      and will claim that I am innocent of the first charge
+      because I am guilty, in one sense at least, of the second charge.
+      That is, my timeline is objective because it precisely because
+      it comes from a strong point of view.
     </p>
-    <p>The timeline have been accused of lack of objectivity and
-    bias.
-    In this post, I will take a careful look at methodology,
-    and claim that I am innocent of the first charge
-    because I am guilty, in one sense at least, of the second charge.
-    That is, my timeline is objective because it precisely because
-    it comes from a strong point of view.
-    </p>
-    <p>Novel as it might seem to some,
-    my methodology is very much mainstream.
-    In this piece, I will refer repeated to Karl Popper's writings
-    on the traditions of science.
-    Popper, in his turn, was much influenced by Einstein.
-    Popper is hailed, correctly, as a champion of objectivity
-    in science.
-    But as you may have seen from the epigraph of this post,
-    Popper's idea of objectivity is quite different in some respects from naive
-    or "common sense" objectivity.
-    </p>
-    <h2>The "common sense" approach</h2>
-<blockquote>"Always approach a case with an absolutely blank mind. It is always an advantage. Form no theories, just simply observe and draw inferences from your observations."<footnote>
-The Adventure of the Cardboard Box</footnote></blockquote>
-<blockquote>"I make a point of never having any prejudices, and of following docilely wherever fact may lead me."<footnote>
-The Reigate Puzzle</footnote></blockquote>
-<blockquote>"It is a capital mistake to theorize before one has data."<footnote>
-A Scandal in Bohemia</footnote></blockquote>
-<blockquote>"When you have eliminated the impossible, whatever remains, no matter how improbable, must be the truth."<footnote>
-The Sign of Four</footnote></blockquote>
+    <h2>The Sherlock Holmes approach</h2>
     <p>Most of those who accuse the timeline of lack of objectivity or of bias,
-    do not state what they mean by those terms,
-    which suggests they are thinking of an approach they expect to be
-    shared as a matter of "common sense".
-    But perhaps they have something in mind like Sherlock Holmes' approach.
-    </p>
-    <p>Many will regard Holmes' methods as just a disciplined version of
-    "common sense", but in fact Holmes is completely wrong.
-    A "blank mind" can observe nothing.
-    There is no "data" without theory, just white noise.
-    And you cannot characterize anything as "impossible",
-    without theorizing in advance of it what kind of fact is possible,
-    and what kind of fact is not.
+      do not state what they mean by those terms,
+      which suggests they are thinking of an approach they expect to be
+      shared as a matter of "common sense".
+      But Einstein assumed his readers idea of discovery
+      would come from Conan Doyle's Sherlock Holmes,
+      and we will follow his lead in starting there.
     </p>
     <p>
-    Using Holmes' methods as stated, he could not find a coconut on Coconut Island.
-    If you read the deduction recorded in the Holmes' canon,
-    you see that each of them requires <b>a lot</b>
-    of theorizing, and not infrequently plain old speculation.
-    For Holmes or anyone else to so much as find the door to
-    Baker Street, they need a theory, explicit or implied,
-    in advance.
-    <h2>Einstein</h3>
-    <blockquote>
-    In the first pages
-of our book we compared the role
-of an investigator
-to that of a detective who, after
-gathering the requisite facts, finds the right solution
-by pure thinking. In one essential this comparison must
-be regarded as highly superficial. Both in life and in
-detective novels the crime is given. The detective must
-look for letters, fingerprints, bullets, guns, but at least
-he knows that a murder has been committed. This is
-not so for a scientist. It should not be difficult to
-imagine someone who knows absolutely nothing about
-electricity, since all the ancients lived happily enough
-without any knowledge of it. Let this man be given
-metal, gold foil, bottles, hard-rubber rod, flannel, in
-short, all the material required for performing our
-three experiments. He may be a very
-cultured person,
-but he will probably put wine into the bottles, use the
-flannel for cleaning, and never once entertain the idea
-of doing the things we have described. For the detective
-the crime is given, the problem formulated: who
-killed Cock Robin? The scientist must, at least in part,
-commit his own crime, as well as carry out the investigation.
-Moreover, his task is not to explain just one
-case, but all phenomena which have happened
-or may
-still happen.
-    </blockquote>
+      In fact Holmes's methods, as stated, are incapable of solving anything
+      but the fictional and artificial problems he encounters.
+      In real life, a "blank mind" can observe nothing.
+      There is no "data" without theory, just white noise.
+      Every "fact" gathered contains a prejudgement about what is
+      relevant and what is not.
+      And you cannot characterize anything as "impossible",
+      without theorizing in advance of it what kind of fact is possible,
+      and what kind of fact is not.
     </p>
+    <p>
+      If you look at the deduction recorded in the Holmes' canon,
+      you see often involve <b>a lot</b>
+      of theorizing, and sometimes outright speculation.
+      To make it a matter of significance what the dogs in "Silver Blaze" did in the night,
+      Holmes requires a theory of canine behavior,
+      and this theory sometimes outpaces the facts by a considerable distance.
+      Is it really true that only dangerous people own
+      dangerous dogs?<footnote>
+      "A dog reflects the family life.
+      Whoever saw a frisky dog in a gloomy family, or a sad dog in a happy one?
+      Snarling people have snarling dogs, dangerous people have dangerous ones."
+      From "The Adventure of the Creeping Man".
+      </footnote>
+    </p>
+    <h2>The Einstein approach</h2>
+    <p>Einstein begins his description of a method 
+    useful outside the pages of a novel at the start
+    of his Chapter II.
+    Many of his less determined readers must have given up
+    the book when they encountered its first paragraph:
+    <blockquote>
+    The following pages contain a dull report of
+The account some be boring very simple experiments.
+not only because the description of experiments is un-
+interesting in comparison with their actual performance,
+but also because the meaning of the experiments does
+not become apparent until theory makes it so. Our
+purpose is to furnish a striking example of the role of
+theory in physics.<footnote>
+    Einstein and Infeld, p. 71.</footnote>
+    </blockquote>
+    <p>
+    </p>
+    <blockquote>
+      In the first pages
+      of our book we compared the role
+      of an investigator
+      to that of a detective who, after
+      gathering the requisite facts, finds the right solution
+      by pure thinking. In one essential this comparison must
+      be regarded as highly superficial. Both in life and in
+      detective novels the crime is given. The detective must
+      look for letters, fingerprints, bullets, guns, but at least
+      he knows that a murder has been committed. This is
+      not so for a scientist. It should not be difficult to
+      imagine someone who knows absolutely nothing about
+      electricity, since all the ancients lived happily enough
+      without any knowledge of it. Let this man be given
+      metal, gold foil, bottles, hard-rubber rod, flannel, in
+      short, all the material required for performing our
+      three experiments. He may be a very
+      cultured person,
+      but he will probably put wine into the bottles, use the
+      flannel for cleaning, and never once entertain the idea
+      of doing the things we have described.
+      For the detective
+      the crime is given, the problem formulated: who
+      killed Cock Robin?
+      The scientist must, at least in part,
+      commit his own crime, as well as carry out the investigation.
+      Moreover, his task is not to explain just one
+      case, but all phenomena which have happened
+      or may
+      still happen.<footnote>
+        Page 78 of Einstein and Infeld.
+      </footnote>
+    </blockquote>
+    <h2>Choosing a "highway"</h2>
+    <blockquote>
+      Through the maze of
+      facts and concepts we had to choose some highway
+      which seemed to us most characteristic and significant.
+      Facts and theories not reached by this road had to be
+      omitted. We were forced, by our general aim, to make
+      a definite choice of facts and ideas. The importance of a
+      problem should not be judged by the number of pages
+      devoted to it. Some essential lines of thought have been
+      left out, not because they seemed to us unimportant,
+      but because they do not lie along the road we have
+      chosen.<footnote>
+        Page 78 of Einstein and Infeld.
+      </footnote>
+    </blockquote>
+    <blockquote>
+      Let me repeat this important point.
+      Scientific theories are not just the
+      results of observation.
+      They are, in the main, the products
+      of myth-making and of tests.
+      Tests proceed partly by way of observation,
+      and observation is thus very important;
+      but its function is not that of producing theories.
+      It plays its role in
+      rejecting, eliminating, and criticizing theories;
+      and it challenges us to produce new myths,
+      new theories which may stand up to these
+      observational tests.<footnote>
+        Karl Popper,
+        <cite>Conjectures and Refutations</cite>,
+        Routledge and Kegan Paul, 2002,
+        p. 172.
+      </footnote>
+    </blockquote>
+    <h2>The end of the journey?</h2>
+    <blockquote>
+    This great mystery story
+is still
+unsolved.
+We
+cannot
+even be sure that it has a final solution. --
+<cite>Einstein and Infeld</cite><footnote>
+Pp. 7-8</footnote>
+</blockquote>
+    </blockquote>
     <h2>Comments, etc.</h2>
     <p>
       The background material for this post is in my
-    <a href="https://jeffreykegler.github.io/personal/timeline_v3">
-    Parsing: a timeline 3.0</a>,
-    and this post may be considered a supplement to "Timelime".
+      <a href="https://jeffreykegler.github.io/personal/timeline_v3">
+        Parsing: a timeline 3.0</a>,
+      and this post may be considered a supplement to "Timelime".
       To learn about Marpa,
       my Earley/Leo-based parsing project,
       there is the
