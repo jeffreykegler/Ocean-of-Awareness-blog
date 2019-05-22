c1d104dd3cc2061210a32173dd6f53235dcb0cf2
Jeffrey Kegler <jeffreykegler@cpan.org>
2019-05-22 15:45:31 -0400

    Work on vgap blog post

--- metasources/vgap.pl
+++ metasources/vgap.pl
@@ -61,7 +61,7 @@ sub do_footnote {
 }
 
 __DATA__
-A Practical use of infinite Lookahead
+Infinite Lookahead and Ruby Slippers
 <html>
   <head>
   </head>
@@ -70,25 +70,41 @@ A Practical use of infinite Lookahead
       marpa_r2_html_fmt --no-added-tag-comment --no-ws-ok-after-start-tag
       -->
     <h2>About this post</h2>
-    <p>Infinite lookahead may not sound like a practical requirement, but it is.
-    The original research into it was motivated by practical interest.<footnote>
-    TODO.  Reference LRR paper, my blog post on it,
-    and my blog post on LRR and Haskell.
+    <p>This post presents a compact, practical example which
+    nicely illustrates the need for both infinite lookahead
+    and Ruby Slippers parsing.
+    Infinite lookahead 
+    is required by any syntax which might of arbitrary length,
+    but whose structure is not certain until the end.
+    Human language has a lot of examples of this<footnote>
+    One example is the sentence "The horse raced past the barn fell".
+    The subclause "raced past the barn" could be anything,
+    and therefore arbitrarily long.
+    In isolation, this sentence may not seem unnatural,
+    a contrived "garden path".
+    But if you imagine it in answer to the question, "Which horse fell?",
+    the construct becomes quite natural.
+    When the expectations are balanced,
+    humans parse these in the same way that Marpa does -- by keeping track
+    of both possibilities until the end,
+    and only then deciding.
     </footnote>
-    And, despite the fact that language writers have trained themselves to work
-    around it, the need keeps popping up.<footnote>
+    and, labor as computer language designers might to avoid it,
+    the need for infinite lookahead keeps popping up 
+    in desirable syntaxes.<footnote>
     Ref to Haskell post.
     </footnote>
     I have just stumbled on a very nice compact example of this.
     </p>
     <h2>About Urbit</h2>
-    <p>Recently, the Urbit community has been generously supporting
-    my work on Marpa parsing.
+    <p>The example of this post arises in the context of a language called Hoon,
+    which is part of the Urbit project.
+    (The Urbit community has, generously, been supporting my work on Hoon.)
     Urbit is an effort to return control of the Internet
     experience to the individual user.
     </p>
     <p>
-    The original Internet and it's predecessors were cosy places.
+    The original Internet and its predecessors were cosy places.
     Users controlled their experience.
     There was authority, but it was so light you
     could forget it was there,
@@ -98,66 +114,73 @@ A Practical use of infinite Lookahead
     into a "brave new world".
     </p>
     <p>
-The modern Internet offers an access to information that
-makes the pure wonder of decades ago seems ridiculous.
-The cost of it has been a transfer
+The Internet has grown in extent beyond previous imaginings,
+and the pure wonder of decades ago seems ridiculous.
+But the price has been a shift
 of power which should be no laughing matter.
 Control of our Internet experience now resides in
 servers,
 run by entities which make no secret of having their own interests.
-Less open, but increasingly obvious, is the single-mindedness with which they pursue
+Less overt, but increasingly obvious, is the single-mindedness with which they pursue
 those interests.
 </p>
 <p>
 And the stakes have risen.
 In the early days,
 we used on the Internet as a supplement in our intellectual lives.
-Today depend on it for our financial and social lives.
+Today, we depend on it for our financial and social lives.
 Today, the server-sphere can be a hostile  place.
 Going forward it may well become a theater of war.
     </p>
     We could try to solve this problem by running our own servers.
     But this is a lot of work, and only leaves us in touch with those
-    willing and able to do that.  In practice, this seems to mean nobody.
+    willing and able to do that.  In practice, this seems to be nobody.
     </p>
     <p>
-Urbit seeks to solve these problems with urbits:
-hassle-free personal servers.
+Urbit seeks to solve these problems with 
+hassle-free personal servers, called urbits.
 Urbits are journaling databases, so they are incorruptable.
 To make sure they can be run anywhere in the cloud,
 they are based on a tiny virtual machine.
 To keep urbits compact and secure,
-Urbit takes on code bloat directly,
-it is a totally original design from a clean slate --
-new protocols,
-and a new stack.
+Urbit takes on code bloat directly:
+Urbit uses a totally original design from a clean slate --
+a new protocol stack, and a new VM called Nock.<footnote>
+In their present form, urbits run on top of Unix and UDP.
+</footnote>
     </p>
     <h2>About Hoon</h2>
     <p>
-    In their present form, urbits run on top of Unix and UDP.
-    Urbit's new VM is called Nock.
-    Its "machine language" is called Nock.
-    Machine languages have evolved -- modern machine languages
-    incorporate memory caching,
-    something the first programmers had to do on their own.
-    Time marches on, and Nock also has garbage collection
-    and arbitrary precision integers.
-    In Nock, all data and code is a "noun",
-    and a noun is either an integer or a tree.
-    This gives Nock a distinct LISP flavor.
+    The "machine language" is trees of arbitrary precision integers.
+    The integers can be interpreted as strings, floats, etc.,
+    as desired.
+    And the trees can be interpreted as lists, giving Nock a distinct LISP flavor.
+    Nock also does its own garbage collection.
+    <footnote>
+    Garbage collection and arbitrary precision may not too high-level
+    for something considered a "machine language",
+    but our concepts evolve.
+    The earliest machine languages required programmers to
+    do their own memory caching and create their own floating
+    point representations,
+    both things we now regard as much too low-level for software
+    to deal with directly.
+    </footnote>
     </p>
     <p>
-    Like traditional
-    machine language, Nock cannot be written directly.
     Traditionally, you had to toggle machine language in physically
     or, more commonly, write it indirectly,
     in assembler or using some higher-level language, like C.
-    <p>Hoon is Urbit's equivalent of C -- it is Urbit's
+    Like traditional
+    machine language, Nock cannot be written directly.
+    Hoon is Urbit's equivalent of C -- it is Urbit's
     "close to the metal" higher level language.
+    </p>
+    <p>
     Not that Hoon looks much like C,
     or for that matter anything else you've ever seen.
     This is a Hoon program that takes an integer argument,
-    call it <tt>n</tt>
+    call it <tt>n</tt>,
     and returns the first <tt>n</tt> counting numbers:
     <pre><tt>
     |=  end=@                                               ::  1
@@ -171,32 +194,42 @@ and a new stack.
     </tt></pre>
     </p>
     <p>
-    We will not learn much Hoon in this post.
-    All we are interesting in, in fact, are Hoon's comments.
     Hoon comments begin with a "<tt>::</tt>" and run until the next
     newline.
     The above Hoon sample uses comments to show line numbers.
-    (For those who want to know more about Hoon,
+    For the sake of our example, comments are the only Hoon syntax we will talk about.
+    (For those who want to learn more about Hoon,
     <a href="https://urbit.org/docs/learn/hoon/">there is a tutorial</a>.)
     </p>
     <p>
     </p>
     <h2>About Hoon comments</h2>
-    <p>For our example, we are interested in Hoon's multi-line Hoon comments.
-    For example,
-    <pre><tt>
-    ::                                                      ::
-    ::::  3e: AES encryption  (XX removed)                  ::
-      ::                                                    ::
-      ::
-    ::                                                      ::
-    ::::  3f: scrambling                                    ::
-      ::                                                    ::
-      ::    ob                                              ::
-      ::
-    </tt> </pre>
     <p>
-    Various types of comments:
+    In the basic Hoon syntax, multi-line comments are free-form.
+    In practice, there are strict, if complex, conventions.
+    In the simplest case, a comment must precede the code it
+    describes, and be at the same indent.
+    These simple cases are called "pre-comments".<footnote>
+    This post attempts to follow standard Hoon terminology, but
+    for the details of Hoon's whitespace conventions,
+    there is settled terminology,
+    and I have invented terms as necessary.
+    The term "pre-comment" is one of those inventions.
+    </footnote>
+    For example, this code contains a pre-comment:
+    <pre><tt>
+	  :: pre-comment 1
+	  [20 (mug bod)]
+    </tt></pre>
+    <p>
+    But some code falls into sequences, and sequences are allowed
+    "inter-comments".
+    In sequences, the pre-comments are indented to match the elements
+    of the sequence,
+    but inter-comments are indented to match the "keyword" of the sequence.
+    The following code has both pre-commments and inter-comments.
+    The intercomments are indented to match the "keyword" digraph: <tt>:~</tt>.
+    (Hoon's keyword digraphs are called "runes".)
     </p>
     <pre><tt>
       :~  [3 7]
@@ -217,6 +250,61 @@ and a new stack.
 	  [5 tay]
       ==
     </tt></pre>
+    <p>Note that the inter-comments in the above, are empty.
+    They are called "breathing comments", and serve to separate,
+    and give some air between, elements of a sequence.
+    </p>
+    <p>
+    Comments can occur at the far left margin -- column 1.
+    These are called meta-comments, because they are allowed
+    to be outside the syntax structure.
+    One common use for meta-comments is "commenting out" code.
+    Note in the above display, the <tt>[4 qax]</tt> and its
+    associated comments are commented out using meta-comments.
+    </p>
+    <p>Lastly, there are "staircase comments", which are used
+    to indicate the larger structure of Hoon sequences and other
+    code.
+    For example,
+    <pre><tt>
+    ::                                                      ::
+    ::::  3e: AES encryption  (XX removed)                  ::
+      ::                                                    ::
+      ::
+    ::                                                      ::
+    ::::  3f: scrambling                                    ::
+      ::                                                    ::
+      ::    ob                                              ::
+      ::
+    </tt> </pre>
+    <p>
+    Rules
+    <ul>
+    <li>Outside of a sequence, inter-comments and pre-comments
+    are the same thing, and are indented the same as the code
+    they precede.
+    </li>
+    <li>Inter-comments, when distinct from pre-comments,
+    must precede them.
+    </li>
+    <li>Staircases can only occur as inter-comments.
+    An inter-comment can contain more than one staircase,
+    but if any staircase occurs in an inter-comment,
+    the whole inter-comment is considered to be a set
+    of staircases.
+    </li>
+    <li>Meta-comments can occur anywhere,
+    but classifying a comment as a meta-comment is a last resort --
+    if a comment can be considered a pre-comment or an inter-comment,
+    then that is how it is classified.
+    </li>
+    </ul>
+    <p>Some of the rules are motivated by a future Hoon tool,
+    <tt>hoonfmt</tt>, which will reformat Hoon in much the same way
+    as <tt>perltidy</tt> reformats Perl, or <tt>indent</tt>
+    reformats C today.
+    </p>
+    </p>
     <p>
     The native compiler allows Hoon comments to be free-form
     but, as the above suggests, in practice Hoon comments are
